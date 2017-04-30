ea5f2e710d05b02816d70808dc63f9e83a80952b
Loi <loichengllc@gmail.com>
2017-04-30 16:37:39 +0000

    ok

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var app = express()
 // set port
 var port = process.env.PORT || 8080
 
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, function () {
