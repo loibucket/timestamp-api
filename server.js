416e36294816b4e2bfae48601f5265f8bdbdfaab
Loi <loichengllc@gmail.com>
2017-04-30 20:44:38 +0000

    timestamp

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
-"user strict"
+"user strict";
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // set port
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
-  res.render('index')
-})
+  res.render('index');
+});
+
+app.get('/:id', function (req, res) {
+
+  var timestamp = decodeURIComponent(req.params.id);
+
+  var dateUnix;
+  var dateNatural;
+  
+  if (!isNaN(timestamp)){
+    timestamp = parseInt(timestamp);
+    dateUnix = timestamp;
+  } else {
+    dateUnix = Date.parse(timestamp);
+  }
+  
+  dateNatural = (new Date(timestamp)).toString();
+  
+  var output = {};
+  
+  if (dateNatural == 'Invalid Date'){
+    output.unix = null;
+    output.natural = null;
+  } else {
+    output.unix = dateUnix;
+    output.natural = dateNatural.substring(4,15);
+  }
+  
+  console.log(JSON.stringify(output));
+  res.json(output);
+  
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port)
+  console.log('Example app listening on port '+port);
 })
